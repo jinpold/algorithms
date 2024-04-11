f80edc2c283df90518b12ea20321ea235c99acdf
jinpold <jinpold@gmail.com>
2024-04-12 08:42:26 +0900

    240412 Boj

---
+++ baekjoon/Boj10699.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Boj10699 {
+    public static void main(String[] args) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date d = new Date();
+        System.out.println(format.format(d));
+    }
+}
